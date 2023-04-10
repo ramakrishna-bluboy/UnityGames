d37a0f2e73c7d562f40e86e09922b95c929d4560
Ram <118962858+ramakrishna-bluboy@users.noreply.github.com>
2023-04-10 12:36:37 +0530

    Added unity lib

--- app/src/main/java/com/blueboy/unitygames/FirstFragment.kt
+++ app/src/main/java/com/blueboy/unitygames/FirstFragment.kt
@@ -1,5 +1,6 @@
 package com.blueboy.unitygames
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.blueboy.unitygames.databinding.FragmentFirstBinding
+import com.unity3d.player.UnityPlayerActivity
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -33,7 +35,29 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            startActivity(
+                Intent(
+                    requireActivity(), UnityPlayerActivity::class.java
+                ).also { it1 ->
+                    it1.putExtra("gameId", "111")
+                    it1.putExtra("game_key", "wretry")
+                    it1.putExtra("playerId", "ram")
+                    //it1.putExtra("TournamentId", battle?.tournamentId)
+                    it1.putExtra("Timer", "")
+                    it1.putExtra(
+                        "auth_key",
+                        "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiODE1NTMyOTM0NzRkOWY4OThjMTEwMGU1ODczYzVjNGVlNTkwMmVkZDM2ODY0NTMyOTA0NjcxZWNjNjNjODhiMDEwMGZiOWY5YmU5NjJhZGYiLCJpYXQiOjE2ODA4NzAzNzkuNDMzMzY1LCJuYmYiOjE2ODA4NzAzNzkuNDMzMzY5LCJleHAiOjE3MTI0OTI3NzkuNDI1OTE2LCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.M7TlpLskrWL3X2GsNkPsnvghOCX7qZeiLo6-b7dJGrpoX5B4hKbzjvX7y8Gzr4-njOM6WISh3QGWVyfXaME6LSOX8jQX7jKsv7LktE8bsS6-fHw59eSrNRwR9xN6_PAK6cgpGTOs80Fra60-OuqfEazgqrJ1omnuoL416Qyko4sTnXmx75GldQUdROfb4ghyZZ0VuEYjnRLidv2b18gaHj_ECUd5KhRcsMb_reznb1saiufMgBKD8IDq9EW3JTKJO8jYSymL0SJk83rYQWZKXt9cyfhEcjanUBzrHGhzcT_9hlvrXqKQUwrMwc6_KHIv-v6dsZBBYPl_N9iWsRLqMGRqDYd3LP1p4Gbp5j0t98vy_QsVtA88o7g6ITaqzdElXAsJ4XiFk-F68qKNSmpepCc7Ch0coxnGAii6xTRXV_f9Wafu_pSDMoNVXb8H3t3uu4HAiweyEMcuQaXHd0I32xu0k5RRbFME8-l8nXrDZXPosAEwwnLLyY_eFirRcLWvpCWfj2q2CPzRcWlTM9GFwKQj8PVP3kRgH3o9QoQIFD5Cxlvu6yPGxmo5kEMc2zUcYCplQHG_wlbcm1bIsB6ERRUIAFS6HfiKJ7TfJuPdNIMsZAxrAFzB_2crTBAhVqfyV5hMAnmjnQ4xyThZP2wNCHFIW0L7eArfwKsaDh9ZO54"
+                    )
+                    it1.putExtra("profileUrl", "")
+                    //it1.putExtra("room_join_fee", battle?.tournamentEntryFees)
+                    it1.putExtra("game_version", "")
+                    it1.putExtra("practice_mode", "true")
+
+                    it1.putExtra("game_image", "")
+                    it1.putExtra("game_banner", "")
+                    it1.putExtra("game_name", "")
+                    it1.putExtra("app_version", BuildConfig.VERSION_NAME)
+                })
         }
     }
 
--- app/src/main/java/com/blueboy/unitygames/MainActivity.kt
+++ app/src/main/java/com/blueboy/unitygames/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.blueboy.unitygames
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.blueboy.unitygames.databinding.ActivityMainBinding
+import com.unity3d.player.UnityPlayerActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,9 +30,27 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        binding.fab.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@MainActivity, UnityPlayerActivity::class.java
+                ).also { it1 ->
+                    it1.putExtra("gameId", "111")
+                    it1.putExtra("game_key", "wretry")
+                    it1.putExtra("playerId", "ram")
+                    //it1.putExtra("TournamentId", battle?.tournamentId)
+                    it1.putExtra("Timer", "")
+                    it1.putExtra("auth_key", "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiODE1NTMyOTM0NzRkOWY4OThjMTEwMGU1ODczYzVjNGVlNTkwMmVkZDM2ODY0NTMyOTA0NjcxZWNjNjNjODhiMDEwMGZiOWY5YmU5NjJhZGYiLCJpYXQiOjE2ODA4NzAzNzkuNDMzMzY1LCJuYmYiOjE2ODA4NzAzNzkuNDMzMzY5LCJleHAiOjE3MTI0OTI3NzkuNDI1OTE2LCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.M7TlpLskrWL3X2GsNkPsnvghOCX7qZeiLo6-b7dJGrpoX5B4hKbzjvX7y8Gzr4-njOM6WISh3QGWVyfXaME6LSOX8jQX7jKsv7LktE8bsS6-fHw59eSrNRwR9xN6_PAK6cgpGTOs80Fra60-OuqfEazgqrJ1omnuoL416Qyko4sTnXmx75GldQUdROfb4ghyZZ0VuEYjnRLidv2b18gaHj_ECUd5KhRcsMb_reznb1saiufMgBKD8IDq9EW3JTKJO8jYSymL0SJk83rYQWZKXt9cyfhEcjanUBzrHGhzcT_9hlvrXqKQUwrMwc6_KHIv-v6dsZBBYPl_N9iWsRLqMGRqDYd3LP1p4Gbp5j0t98vy_QsVtA88o7g6ITaqzdElXAsJ4XiFk-F68qKNSmpepCc7Ch0coxnGAii6xTRXV_f9Wafu_pSDMoNVXb8H3t3uu4HAiweyEMcuQaXHd0I32xu0k5RRbFME8-l8nXrDZXPosAEwwnLLyY_eFirRcLWvpCWfj2q2CPzRcWlTM9GFwKQj8PVP3kRgH3o9QoQIFD5Cxlvu6yPGxmo5kEMc2zUcYCplQHG_wlbcm1bIsB6ERRUIAFS6HfiKJ7TfJuPdNIMsZAxrAFzB_2crTBAhVqfyV5hMAnmjnQ4xyThZP2wNCHFIW0L7eArfwKsaDh9ZO54")
+                    it1.putExtra("profileUrl", "")
+                    //it1.putExtra("room_join_fee", battle?.tournamentEntryFees)
+                    it1.putExtra("game_version", "")
+                    it1.putExtra("practice_mode", "true")
+
+                    it1.putExtra("game_image", "")
+                    it1.putExtra("game_banner", "")
+                    it1.putExtra("game_name", "")
+                    it1.putExtra("app_version", BuildConfig.VERSION_NAME)
+                })
         }
     }
 
